f718826028f82078c56d9711f9f44b6421654b8d
David Eker <admin@dialectsoftware.com>
2016-01-07 22:45:42 -0500

    Add LICENSE file to project

--- DialectSoftware.Semantics.WordReader/WordReader.cs
+++ DialectSoftware.Semantics.WordReader/WordReader.cs
@@ -5,6 +5,12 @@ using System.IO;
 using System.Threading;
 using System.Collections.Generic;
 
+// ******************************************************************************************************************
+/// * Copyright (c) 2011 Dialect Software LLC                                                                        *
+/// * This software is distributed under the terms of the Apache License http://www.apache.org/licenses/LICENSE-2.0  *
+/// *                                                                                                                *
+/// ******************************************************************************************************************
+
 namespace DialectSoftware.Semantics
 {
     //TODO:Finish
